0f3386aebb51b018c46bba0f205b97c30e7c13fc
Adolfo Leon Canizalez <acanizalezm@devco.com.co>
2023-05-29 08:48:45 -0500

    First Exercises

---
+++ TestDevco/TestDevco/Exercises/F01/BarReservation.cs
@@ -0,0 +1,20 @@
+﻿namespace TestDevco.Exercises.F01
+{
+    public class BarReservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanUpdateReservation(User user)
+        {
+            if (user.IsAdmin) return true;
+            if (MadeBy.Id == user.Id) return true;
+            return false;
+        }
+    }
+
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ TestDevco/TestDevco/Exercises/F02/BarBasicOperations.cs
@@ -0,0 +1,22 @@
+﻿namespace TestDevco.Exercises.F02
+{
+    public class BarBasicOperations
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int MinValue(int a, int b)
+        {
+            return (a < b) ? a : b;
+        }
+
+        public IEnumerable<int> GetEvenNumbers(int limit)
+        {
+            for (var i = 0; i <= limit; i++)
+                if (i % 2 == 0)
+                    yield return i;
+        }
+    }
+}
