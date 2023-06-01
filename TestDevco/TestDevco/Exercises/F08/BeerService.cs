91d556907f23591f9171a7de934494b295252e76
Adolfo Leon Canizalez <acanizalezm@devco.com.co>
2023-06-01 00:41:36 -0500

    Adding moq

---
+++ TestDevco/TestDevco/Exercises/F08/BeerService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace TestDevco.Exercises.F08
+{
+    public class BeerService
+    {
+        
+        public string ReadBeers()
+        {
+            var str = File.ReadAllText("corona.txt");
+            var objBeer = JsonConvert.DeserializeObject<Beer>(str);
+            if (objBeer == null)
+                return "Error getting beer specification.";
+            return objBeer.Title;
+        }
+
+    }
+
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal PercentageAlcohol { get; set; }
+    }
+}
