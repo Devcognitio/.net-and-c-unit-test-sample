4fea97916c3a3155751e99790dfb7a6c43b752dc
Adolfo Leon Canizalez <acanizalezm@devco.com.co>
2023-06-01 22:23:31 -0500

    cambios de la primera clase

---
+++ TestDevco/TestDevco.UnitTests/Exercises/F01/BarReservationTests.cs
@@ -0,0 +1,48 @@
+﻿using TestDevco.Exercises.F01;
+
+namespace TestDevco.UnitTests.Exercises.F01
+{
+    [TestFixture]
+    public class BarReservationTests
+    {
+        [Test]
+        public void CanUpdateReservation_AdminUpdating_ReturnTrue()
+        {
+            // Arrange (organizar)
+            var objReservation = new BarReservation();
+            var user = new User { IsAdmin = true };
+
+            // Act
+            var result = objReservation.CanUpdateReservation(user);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanUpdateReservation_SameUserUpdating_ReturnTrue()
+        {
+            var user = new User();
+            var objReservation = new BarReservation() { MadeBy = user };
+
+            var result = objReservation.CanUpdateReservation(user);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanUpdateReservation_OtherUserUpdating_RertunFalse()
+        {
+            var user = new User();
+            var objReservation = new BarReservation();
+
+            var result = objReservation.CanUpdateReservation(user);
+
+            Assert.That(result, Is.False);
+            //Assert.That(!result, Is.True);
+        }
+
+    }
+}
---
+++ TestDevco/TestDevco.UnitTests/Exercises/F02/BarBasicOperationsTests.cs
@@ -0,0 +1,19 @@
+﻿using TestDevco.Exercises.F02;
+
+namespace TestDevco.UnitTests.Exercises.F02
+{
+    [TestFixture]
+    public class BarBasicOperationsTests
+    {
+        [Test]
+        public void Sum_SumValues_ReturnSumValue()
+        {
+            var objBarBasicOperations = new BarBasicOperations();
+
+            var result = objBarBasicOperations.Sum(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+            Assert.That(result == 3);
+        }
+    }
+}
