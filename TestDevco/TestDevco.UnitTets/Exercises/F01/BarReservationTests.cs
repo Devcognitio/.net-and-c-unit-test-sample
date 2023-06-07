886f5561047773b9b101b44093e36e32ddabf08a
Adolfo Leon Canizalez <acanizalezm@devco.com.co>
2023-06-07 06:56:33 -0500

    Primera clase

---
+++ TestDevco/TestDevco.UnitTets/Exercises/F01/BarReservationTests.cs
@@ -0,0 +1,53 @@
+﻿using TestDevco.Exercises.F01;
+
+namespace TestDevco.UnitTets.Exercises.F01
+{
+    [TestFixture]
+    public class BarReservationTests
+    {
+        // NombreDelMetodo_Escenario_ResultadoEsperado
+
+        [Test]
+        public void CanUpdateReservation_AdminUpdating_ReturnTrue()
+        {
+            // triple A --> Arrange (organizar el escenario), Act (Actuar), Assert (Afirmaciones)
+
+            // Arrage
+            var objReservation = new BarReservation();
+            var adminUser = new User { IsAdmin = true };
+
+            // Act
+            var result = objReservation.CanUpdateReservation(adminUser);
+
+            // Asserts
+            //Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            //Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanUpdateReservation_SameUserUpdating_ReturnTrue()
+        {
+            var user = new User();
+            var objReservation = new BarReservation();
+            objReservation.MadeBy = user;
+
+            var result = objReservation.CanUpdateReservation(user);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanUpdateReservation_OtherUserUpdating_ReturnFalse()
+        {
+            var user = new User();
+            var objReservation = new BarReservation();
+
+            var result = objReservation.CanUpdateReservation(user);
+
+            Assert.That(result, Is.False);
+            Assert.IsFalse(result);
+        }
+
+    }
+}
--- TestDevco/TestDevco/Exercises/F01/BarReservation.cs
+++ TestDevco/TestDevco/Exercises/F01/BarReservation.cs
@@ -2,13 +2,21 @@
 {
     public class BarReservation
     {
-        public User MadeBy { get; set; }
+        public User MadeBy { get; set; } = new User();
 
         public bool CanUpdateReservation(User user)
         {
+            return user.IsAdmin || MadeBy.Id == user.Id;
+
+            /*
+            var result = false;
+            if (user.IsAdmin || MadeBy.Id == user.Id) { result = true; }
+            return result;
+
+            
             if (user.IsAdmin) return true;
             if (MadeBy.Id == user.Id) return true;
-            return false;
+            return false;*/
         }
     }
 
